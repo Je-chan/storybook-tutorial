223bd3f6ab6b2886155eefd78e50332ec024e29b
liebe <liebe@gridwiz.com>
2022-02-23 19:09:50 +0900

    CREATE : stroybook-tutorial init

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
+    '@vue/prettier',
+  ],
+  ignorePatterns: [
+    '.vscode/*',
+    'dist/*',
+    'assets/*',
+    'node_modules/*',
+    '**/index.html',
+    '**/index.*.html',
+    '**/assets/*.html',
+  ],
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  overrides: [
+    {
+      files: ['cypress/integration/**.spec.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended'],
+    },
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
